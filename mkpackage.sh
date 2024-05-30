6a97ff6f7a9dd9227d73a2653a596e9f2ecc6565
Bobby White <bobby.white@bobbywh-ltm4bf3.internal.salesforce.com>
2024-05-30 11:28:47 -0400

    Added Scratch org config to package version create

--- mkpackage.sh
+++ mkpackage.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 #sf package create --name "VoiceCallFields" --path force-app --package-type Managed
-sf package version create --package "VoiceCallFields" --code-coverage --installation-key-bypass --wait 10
+sf package version create --package "VoiceCallFields" --code-coverage --installation-key-bypass --definition-file config/project-scratch-def.json --wait 10
 
