97f2a00fc57708152fe0081b065395b0bd59badc
Bobby White <bobby.white@bobbywh-ltm4bf3.internal.salesforce.com>
2024-05-29 22:00:11 -0400

    50 custom fields on VoiceCall

---
+++ orgInit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Exit on error!
+set -euxo pipefail
+
+function prop {
+    grep "${1}" project.properties|cut -d'=' -f2
+}
+#create scratch org
+sfdx force:org:create -f config/project-scratch-def.json -s -a VOICEEXT username="$(prop 'user.admin' )" --durationdays 28
+
+
+sfdx force:org:open
