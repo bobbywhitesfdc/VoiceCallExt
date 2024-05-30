3fd80ded7323147986c2d035473c3f612990e7a8
Bobby White <bobby.white@bobbywh-ltm4bf3.internal.salesforce.com>
2024-05-30 14:04:10 -0400

    fix deploy script errors

--- deployTest.sh
+++ deployTest.sh
@@ -6,8 +6,7 @@ function prop {
     grep "${1}" project.properties|cut -d'=' -f2
 }
 #create TARGET scratch org
-sfdx force:org:create -f config/project-scratch-def.json -s -a TARGET username="$(prop 'target.admin' )" 
+sfdx force:org:create -f config/project-scratch-def.json -a TARGET username="$(prop 'target.admin' )" 
 
 # deploy the package there
-sf package install --package "VoiceCallFields" -o TARGET
-
+sf package install --package "VoiceCallFields@0.1.0-1" -o TARGET -w 10
