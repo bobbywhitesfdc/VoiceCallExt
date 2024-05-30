ff674b310dd34c6c85206b13300393c257f1a9df
Bobby White <bobby.white@bobbywh-ltm4bf3.internal.salesforce.com>
2024-05-30 06:28:50 -0400

    Removed reference to deleted CustField1

--- mkpackage.sh
+++ mkpackage.sh
@@ -2,6 +2,6 @@
 # Exit on error!
 set -euxo pipefail
 
-sf package create --name "VoiceCallFields" --path force-app --package-type Managed
+#sf package create --name "VoiceCallFields" --path force-app --package-type Managed
 sf package version create --package "VoiceCallFields" --code-coverage --installation-key-bypass --wait 10
 
